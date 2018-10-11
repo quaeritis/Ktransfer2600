0ea0936921cf13b4642838fc8f89ae8a578506fd
Dennis Heffels <dennis.heffels@rwth-aachen.de>
2018-10-11 17:41:24 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'Ktransfer2600=sweepVlist.__main__:main'
+            'Ktransfer2600=Ktransfer2600.__main__:main'
         ],
         'gui_scripts': [
-            'Ktransfer2600_gui=sweepVlist.__main__:main'
+            'Ktransfer2600_gui=Ktransfer2600.__main__:main'
         ]
     },
     install_requires=[
